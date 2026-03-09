6be39c49b1e489b6c33d6673c3893eb4087d71e1
James <support@mytitan.co.uk>
2026-03-09 09:07:30 +0000

    fix(realtime): import events module in jobs module

--- api/src/jobs/jobs.module.ts
+++ api/src/jobs/jobs.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuditModule } from "../audit/audit.module";
 import { AutomationsModule } from "../automations/automations.module";
+import { EventsModule } from "../events/events.module";
 import { NotificationsModule } from "../notifications/notifications.module";
 import { TemplatesModule } from "../templates/templates.module";
 import { JobsController } from "./jobs.controller";
 import { JobsService } from "./jobs.service";
 
 @Module({
-  imports: [AuditModule, TemplatesModule, NotificationsModule, AutomationsModule],
+  imports: [AuditModule, TemplatesModule, NotificationsModule, AutomationsModule, EventsModule],
   controllers: [JobsController],
   providers: [JobsService],
 })
