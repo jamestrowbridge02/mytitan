876260d55288b98e4bb6c3819e316df076f0a0c5
James <support@mytitan.co.uk>
2026-03-13 10:47:24 +0000

    fix(marketing): remove duplicated shell wrapping

--- marketing/pages/_app.tsx
+++ marketing/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app';
-import MarketingShell from "../components/layout/MarketingShell";
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <MarketingShell><Component {...pageProps} /></MarketingShell>;
+  return <Component {...pageProps} />;
 }
